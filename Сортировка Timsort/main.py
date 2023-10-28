f970cc7a4dfb847f6359d07cd6e601e845b4cbcf
Kartoshka <kartoshka0811@gmail.com>
2023-10-28 13:36:20 +0300

    up

--- Сортировка Timsort/main.py
+++ Сортировка Timsort/main.py
@@ -78,6 +78,93 @@ def timsort(array):
 
 
 import time
+
+
+#3.2.1 внешняя сортировка (объединение. пересечение, разность, симметрич.разность)
+#для объединения
+print("3.2.1 внешняя сортировка (объединение. пересечение, разность, симметрич.разность):")
+
+#записываем два файла
+f = open("mergeSort/mass_3.2.1(1)")
+from random import randint
+a = [randint(0, 4000) for i in range(2600)]
+timsort(a)
+with open('mergeSort/mass_3.2.1(1)', 'w') as f:
+    f.write(str(a))
+
+f = open("mergeSort/mass_3.2.1(2)")
+b = [randint(0, 4000) for i in range(2600)]
+timsort(b)
+with open('mergeSort/mass_3.2.1(2)', 'w') as f:
+    f.write(str(b))
+
+#выполняем остальное задание
+#для объединения
+
+c1 = []
+for i in range(len(a)-1):
+    c1.append(a[i])
+for j in range(len(b)-1):
+    c1.append(b[j])
+
+start = time.time()
+timsort(c1)
+end = time.time() - start
+print("time for 1st " + str(end) + " sec")
+
+
+#для пересечения / перечисления  только те числа, которые есть и там и там
+
+c2 = []
+for i in a:
+    for j in b:
+        if i == j:
+            c2.append(i)
+            break
+
+
+start = time.time()
+timsort(list(c2))
+end = time.time() - start
+print("time for 2nd " + str(end) + " sec")
+
+
+#для разности (числа, принадлежащие первому множеству, но не второму)
+start = time.time()
+c3 = []
+sov = 0
+for i in range(len(a)-1):
+    for j in range(len(a)-1):
+        if a[i] == b[j]:
+            sov += 1
+    if sov == 0:
+        c3.append(a[i])
+    sov = 0
+
+timsort(list(c3))
+end = time.time() - start
+print("time for 3rd " + str(end) + " sec")
+
+
+#для симметрич.разности (объединение разностей множеств)
+start = time.time()
+a = set(a)
+b = set(b)
+c4 = a.symmetric_difference(b)
+timsort(list(c4))
+end = time.time() - start
+print("time for 4th " + str(end) + " sec")
+
+f = open("mergeSort/mass_3.2.1")
+with open('mergeSort/mass_3.2.1', 'w') as f:
+    f.write( str(c1)+ "\n" + str(c2) + "\n"
+             + str(c3) + "\n" + str(c4))
+
+
+
+
+
+print()
 #3.2.2. Внутренняя сортировка.  Отсортировать два массива в один
 print("3.2.2. Внутренняя сортировка.  Отсортировать два массива в один")
 start = time.time()
@@ -95,3 +182,34 @@ with open('mergeSort/mass_3.2.2', 'w') as f:
     f.write(str(m1) + "\n" + str(m2) + "\n" + "\n" + str(m3))
 end = time.time() - start
 print("timeInt = " + str(end) + " sec")
+
+
+#3.2.3. Внутренняя сортировка. Дана целочисленная квадратная матрица размером n=5000. Используя указанные методы,
+# упорядочить в ней значения по возрастанию в строках и столбцах. Определить время работы, число сравнений и перестановок.
+print("3.2.3. Внутренняя сортировка. Дана целочисленная квадратная матрица")
+start = time.time()
+
+from random import randint
+
+# ввод матрицы
+matrix = [0] * 10
+for i in range(10):
+    matrix[i] = [randint(0, 11) for i in range(10)]
+
+
+# сортировка матриццы
+    # сортировка строк матрицы
+for i in range(len(matrix)):
+    for j in range(len(matrix[i])):
+        timsort(matrix[i])
+    # сортировка столбцов матрицы
+for i in range(len(matrix)):
+    timsort(matrix)
+
+with open('mergeSort/mass_3.2.3', 'w') as f:
+    for i in range(len(matrix)):
+        t = str(matrix[i])+ "\n"
+        f.write(t)
+
+end = time.time() - start
+print("time_3.2.3 = " + str(end) + " sec")
